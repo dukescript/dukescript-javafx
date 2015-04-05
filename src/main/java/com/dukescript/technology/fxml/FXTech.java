591174f0e7eefa1c9290d040512b7b96a8fade80
Eppleton IT <seal@dukescript.com>
2015-04-05 14:41:04 +0200

    Adding CDDL license headers to source files

--- src/main/java/com/dukescript/technology/fxml/FXTech.java
+++ src/main/java/com/dukescript/technology/fxml/FXTech.java
@@ -1,5 +1,29 @@
 package com.dukescript.technology.fxml;
 
+/*
+ * #%L
+ * DukeScript and FXML UI
+ * %%
+ * Copyright (C) 2015 Eppleton IT Consulting
+ * %%
+ * COMMON DEVELOPMENT AND DISTRIBUTION LICENSE (CDDL) Version 1.0
+ * 
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the Common Development and Distrubtion License as
+ * published by the Sun Microsystems, either version 1.0 of the
+ * License, or (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Lesser Public License for more details.
+ * 
+ * You should have received a copy of the Common Development and Distrubtion
+ * License along with this program.  If not, see
+ * <http://www.gnu.org/licenses/gpl-1.0.html>.
+ * #L%
+ */
+
 import net.java.html.json.Function;
 import net.java.html.json.Model;
 import net.java.html.json.Property;
