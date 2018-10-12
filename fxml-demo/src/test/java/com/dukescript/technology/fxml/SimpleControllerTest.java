9f40c6da64746fe2bca83f5307105b4402d81f2d
Eppleton IT <seal@dukescript.com>
2018-10-12 06:24:37 +0200

    Giving all the files MIT license

--- fxml-demo/src/test/java/com/dukescript/technology/fxml/SimpleControllerTest.java
+++ fxml-demo/src/test/java/com/dukescript/technology/fxml/SimpleControllerTest.java
@@ -6,21 +6,23 @@ package com.dukescript.technology.fxml;
  * %%
  * Copyright (C) 2015 Eppleton IT Consulting
  * %%
- * COMMON DEVELOPMENT AND DISTRIBUTION LICENSE (CDDL) Version 1.0
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
  * 
- * This program is free software: you can redistribute it and/or modify
- * it under the terms of the Common Development and Distrubtion License as
- * published by the Sun Microsystems, either version 1.0 of the
- * License, or (at your option) any later version.
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
  * 
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Lesser Public License for more details.
- * 
- * You should have received a copy of the Common Development and Distrubtion
- * License along with this program.  If not, see
- * <http://www.gnu.org/licenses/gpl-1.0.html>.
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
  * #L%
  */
 
